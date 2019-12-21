6e82da211dc4b9bcf4524a48c91af10b16d0abe3
AppLab <applab@Roberts-MacBook-Pro.local>
2019-12-21 10:03:56 -0400

    testing alert devices in mobile

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,51 +1,53 @@
 import React from 'react';
 import Webcam from "react-webcam";
 import './App.css';
-
-const FACING_MODE_USER = "user";
-const FACING_MODE_ENVIRONMENT = "environment";
-
-const videoConstraints = { 
-  facingMode: FACING_MODE_USER
-};
  
 export default function App () {
-  const [facingMode, setFacingMode] = React.useState(FACING_MODE_USER);
-
   const webcamRef = React.useRef(null);
+  const [deviceId, setDeviceId] = React.useState({});
+  const [devices, setDevices] = React.useState([]);
 
-  const handleClick = React.useCallback(() => {
-    setFacingMode(
-      prevState =>
-        prevState === FACING_MODE_USER
-          ? FACING_MODE_ENVIRONMENT
-          : FACING_MODE_USER
-    );
-  }, []);
+  const handleDevices = React.useCallback(
+    mediaDevices =>
+      setDevices(mediaDevices.filter(({ kind }) => kind === "videoinput")),
+    [setDevices]
+  );
 
   const capture = React.useCallback(
     () => {
       const imageSrc = webcamRef.current.getScreenshot();
-      console.log(imageSrc);
+      alert('Imagen guardada');
     },
     [webcamRef]
   );
+
+  React.useEffect(
+    () => {
+      navigator.mediaDevices.enumerateDevices().then(handleDevices);
+    },
+    [handleDevices]
+  );
+
+  const showDevices = () => {
+      let devicesList = devices.map(device => device.label);
+      alert(devicesList);
+  }
     
  
   return (
     <div className="container">
       <div id="vid_container">
       <Webcam
-        audio={false}
+        audio={false} 
+        videoConstraints={{ deviceId }}
         ref={webcamRef}
         screenshotFormat="image/jpeg"
-        videoConstraints={videoConstraints}
       />
         <div id="video_overlay"></div>
     </div>
       
        <div id="controls">
-          <button className="leftButton" onClick={handleClick} name="switch Camera" type="button" aria-pressed="false">Abbruch</button>
+          <button className="leftButton" onClick={showDevices} name="switch Camera" type="button" aria-pressed="false">Abbruch</button>
           <button className="takePhotoButton" onClick={capture} name="take Photo" type="button"></button>
           <button className="rightButton" name="toggle FullScreen" type="button" aria-pressed="false">Fertig</button>
       </div>
