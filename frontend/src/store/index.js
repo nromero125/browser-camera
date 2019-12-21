5700806cb7f8081370d712941b9282c74d10866d
AppLab <applab@Roberts-MacBook-Pro.local>
2019-12-21 17:25:47 -0400

    fixing error with redux_tools

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -8,6 +8,6 @@ const store = createStore(
     rootReducer,
     initialState,
     compose(applyMiddleware(thunk),
-        window.__REDUX_DEVTOOLS_EXTENSION__&& window.__REDUX_DEVTOOLS_EXTENSION__()));
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__() || compose));
 
 export default store;
