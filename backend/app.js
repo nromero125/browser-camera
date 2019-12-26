e3325f90236f89db41b2b66814217790de4c2f3e
Nelson Romero <nromero125@gmail.com>
2019-12-25 23:16:48 -0400

    increased the limit in the body parser to send the base64 img

--- backend/app.js
+++ backend/app.js
@@ -1,19 +1,19 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
 const helmet = require('helmet');
 
+const users = require('./routes/users');
+const pdf = require('./routes/pdf');
+const app = express();
 
-var users = require('./routes/users');
-
-var app = express();
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json({limit: '200mb'}));
+app.use(bodyParser.urlencoded({limit: '200mb', extended: true}));
 app.use(cookieParser());
 app.use(helmet());
 
 app.use('/api/v1/users', users);
+app.use(pdf );
 
 module.exports = app;
