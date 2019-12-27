a01233047e52fac96360c133487d97a42fe80b23
Nelson Romero <nromero125@gmail.com>
2019-12-27 01:20:03 -0400

    updated gitignore and updated main component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,37 +1,40 @@
 import React,{useEffect} from 'react';
 import Webcam from "react-webcam";
-import CameraDropdown from './Components/CameraDropdown';
+import { Modal, Button } from 'antd';
+import ConfirmModal from './Components/ConfirmModal';
 import { useDispatch, useSelector } from "react-redux";
-import {getDevices, switchFacingMode} from './actions/camera';
+import {switchFacingMode, sendPhoto, takePhoto, cancelPhoto} from './actions/camera';
 import './App.css';
  
 
-const useFetching = (action, dispatch) => {
-  useEffect( () => {
-      dispatch(action);
-  }, []);
-};
-
 export default function App () {
-
-  /* const devices = useSelector(state => state.camera.devices);
-  let deviceId = useSelector(state => state.camera.deviceId); */
   let facingMode = useSelector(state => state.camera.facingMode);
+  let cameraState = useSelector(state => state.camera);
+  console.log(cameraState);
 
   const dispatch = useDispatch();
-  useFetching(getDevices(), dispatch);
 
   const webcamRef = React.useRef(null);
 
   const capture = React.useCallback(
     () => {
       const imageSrc = webcamRef.current.getScreenshot();
-      alert('Imagen guardada');
+      dispatch(takePhoto(imageSrc));
     },
     [webcamRef]
   );
 
-  const stop = () => {
+  const deletePhoto = () => {
+    dispatch(cancelPhoto());
+  }
+
+  const successModal = () => {
+    Modal.success({
+      content: 'Sent Successfully',
+    });
+  }
+  //Switch Camera
+  /* const stop = () => {
     let stream = webcamRef.current.video.srcObject;
     const tracks = stream.getTracks();
     
@@ -42,25 +45,27 @@ export default function App () {
   const changeFacingMode = (facingMode) => {
       stop();
       dispatch(switchFacingMode(facingMode));
-  }
+  } */
  
   return (
     <div className="container">
       <div id="vid_container">
-      <Webcam
-        audio={false} 
-        videoConstraints={{ facingMode: facingMode }}
-        ref={webcamRef}
-        screenshotFormat="image/jpeg"
-      />
+        {
+          cameraState.taked ? <img src={cameraState.photo} /> : 
+          <Webcam
+          audio={false} 
+          videoConstraints={{ facingMode: facingMode }}
+          ref={webcamRef}
+          screenshotFormat="image/jpeg"
+        />  
+        }
         <div id="video_overlay"></div>
     </div>
       
        <div id="controls">
-         {/* <CameraDropdown className="leftButton" devices={devices}/> */}
-          <button className="leftButton" onClick={() => changeFacingMode(facingMode)} name="switch Camera" type="button" aria-pressed="false">Abbruch</button>
+          <button className="leftButton" onClick={deletePhoto} name="switch Camera" type="button" aria-pressed="false">Abbruch</button>
           <button className="takePhotoButton" onClick={capture} name="take Photo" type="button"></button>
-          <button className="rightButton" name="toggle FullScreen" type="button" aria-pressed="false">Fertig</button>
+          <ConfirmModal />
       </div>
     </div>
   );
